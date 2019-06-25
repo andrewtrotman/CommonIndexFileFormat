5692c0db0514c1461af785a4b8bc8c85434ed9b4
AndrewTrotman <andrew@cs.otago.ac.nz>
2019-06-26 11:17:19 +1200

    added some robustness

--- dump_ciff_lin.cpp
+++ dump_ciff_lin.cpp
@@ -11,6 +11,8 @@
 
 	@brief Read and dump an index in Jimmy Lin's Common Index File Format.
 */
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -52,7 +54,14 @@ size_t read_entire_file(const std::string &filename, std::string &into)
 int main(int argc, const char *argv[])
 	{
 	std::string file;
-	size_t file_size = read_entire_file(argv[1], file);
+	size_t file_size;
+
+	if (argc != 2)
+		exit(printf("Usage:%s <infile.pb>\n", argv[0]));
+
+	if ((file_size = read_entire_file(argv[1], file)) == 0)
+		exit(printf("Can't read file:%s\n", argv[1]));
+
 	JASS::ciff_lin source((uint8_t *)&file[0], file_size);
 
 	for (const auto &posting : source)
@@ -60,6 +69,8 @@ int main(int argc, const char *argv[])
 		std::cout.write((char *)posting.term.start, posting.term.length);
 		std::cout << " " << posting.document_frequency << " " << posting.collection_frequency << "\n";
 		}
+	if (source.status == JASS::ciff_lin::FAIL)
+		exit(printf("File is not in the correct format\n"));
 
 	return 0;
 	}
